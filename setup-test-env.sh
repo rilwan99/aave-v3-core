07fa974a585e427e9363500af2194850afde634c
rilwan99 <rilwanwild@gmail.com>
2023-01-30 17:55:54 +0800

    initial commit

--- setup-test-env.sh
+++ setup-test-env.sh
@@ -13,15 +13,15 @@
 
 echo "[BASH] Setting up testnet environment"
 
-if [ ! "$COVERAGE" = true ]; then
-    # remove hardhat and artifacts cache
-    npm run ci:clean
+# if [ ! "$COVERAGE" = true ]; then
+#     # remove hardhat and artifacts cache
+#     npm run ci:clean
 
-    # compile @aave/core-v3 contracts
-    npm run compile
-else
-    echo "[BASH] Skipping compilation to keep coverage artifacts"
-fi
+#     # compile @aave/core-v3 contracts
+#     npm run compile
+# else
+#     echo "[BASH] Skipping compilation to keep coverage artifacts"
+# fi
 
 # Copy artifacts into separate directory to allow
 # the hardhat-deploy library load all artifacts without duplicates 
